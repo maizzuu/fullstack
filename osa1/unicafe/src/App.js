105e52ac730f7f62eb4263fb9da51b7408ac7883
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-01-11 18:34:57 +0200

    1.10

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   if (props.all === 0) {
     return (
@@ -14,17 +22,16 @@ const Statistics = (props) => {
       <div>
         <h1>statistics</h1>
         <p>
-          good {props.good}<br></br>
-          neutral {props.neutral}<br></br>
-          bad {props.bad}<br></br>
-          all {props.all}<br></br>
-          average {(props.good*1+props.bad*-1)/props.all}<br></br>
-          positive {(props.good/props.all)*100} %
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.all} />
+          <StatisticLine text="average" value={(props.good*1+props.bad*-1)/props.all} />
+          <StatisticLine text="positive" value={(props.good/props.all)*100 + " %"} />
         </p>
       </div>
     )
   }
-  
 }
 
 const Button = (props) => {
