63e457a6931ba33b70f2a36bba4dbea24e361d12
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-01-25 13:25:23 +0200

    bug fixes

--- osa2/countries/src/components/ShowCountries.js
+++ osa2/countries/src/components/ShowCountries.js
@@ -2,14 +2,15 @@ import React from "react";
 import Country from "./Country";
 
 const ShowCountries = ({ countries, filter, setNewFilter }) => {
-  const handleClick = (country) => {
-    setNewFilter(country.name.common);
+  const handleClick = (countryName) => {
+    setNewFilter(countryName);
   };
 
   if (filter === "") {
     return null;
   } else {
     if (countries.length > 10) {
+      console.log("more than ten");
       return <div>Too many matches</div>;
     }
     if (countries.length === 1) {
@@ -20,12 +21,13 @@ const ShowCountries = ({ countries, filter, setNewFilter }) => {
       );
     }
     if (countries.length < 10) {
+      console.log("less than ten");
       return (
         <div>
           {countries.map((country) => (
             <p key={country.name.common}>
               {country.name.common}{" "}
-              <button onClick={() => handleClick((country = country))}>
+              <button onClick={() => handleClick(country.name.common)}>
                 show
               </button>
             </p>
