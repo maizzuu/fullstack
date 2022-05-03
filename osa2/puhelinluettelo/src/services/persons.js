6867a801337ffe0a27a4978ad8293e3e16380513
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-05-03 16:04:38 +0300

    3.11 updated url

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://gentle-lowlands-09073.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
