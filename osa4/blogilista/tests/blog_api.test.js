596f6beaa5f962b895fad61c22128c9c31db63ef
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-05-06 10:41:36 +0300

    4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -33,6 +33,12 @@ test("all blogs are returned", async () => {
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
+test("id field name is correct", async () => {
+  const response = await api.get("/api/blogs")
+
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
