999d5ab0d5f92956e1d963982b96766f89d1ba87
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-05-10 17:00:44 +0300

    4.20

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -25,9 +25,13 @@ app.use(cors())
 app.use(express.static("build"))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", userRouter)
 app.use("/api/login", loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
