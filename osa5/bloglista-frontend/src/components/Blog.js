342fc06ddaa58483b09e652b0436bc6b3fe0c189
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-07-29 08:56:26 +0300

    5.7

--- osa5/bloglista-frontend/src/components/Blog.js
+++ osa5/bloglista-frontend/src/components/Blog.js
@@ -1,7 +1,30 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [open, setOpen] = useState(false);
 
-export default Blog
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setOpen(!open)}>{open ? "hide" : "view"}</button>
+      {open && (
+        <>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.user.name}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
