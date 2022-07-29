21862f1a8f5b4a7832d7516604c3f9e3e90c33ea
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-07-29 10:31:56 +0300

    5.11, 5.12

--- osa5/bloglista-frontend/src/components/Blog.js
+++ osa5/bloglista-frontend/src/components/Blog.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import blogService from "../services/blogs";
+import PropTypes from "prop-types";
 
 const Blog = ({ blog, showRemove, handleRemove }) => {
+  Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+    showRemove: PropTypes.bool.isRequired,
+    handleRemove: PropTypes.func.isRequired,
+  };
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
--- osa5/bloglista-frontend/src/components/Notification.js
+++ osa5/bloglista-frontend/src/components/Notification.js
@@ -1,4 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 const Notification = ({ message, type }) => {
+  Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  };
+
   if (message === null) {
     return null;
   }
