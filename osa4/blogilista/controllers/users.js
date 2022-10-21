2c82482eaceef04fad72d3595f7436fa6fb9f94e
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-10-21 12:08:38 +0300

    7.9

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcrypt")
-const usersRouter = require("express").Router()
-const User = require("../models/user")
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({}).populate("blogs", {
@@ -8,53 +8,53 @@ usersRouter.get("/", async (request, response) => {
     title: 1,
     author: 1,
     _id: 1,
-  })
-  response.json(users)
-})
+  });
+  response.json(users);
+});
 
 usersRouter.post("/", async (request, response) => {
-  const { username, name, password } = request.body
+  const { username, name, password } = request.body;
 
   if (!username) {
     return response
       .status(400)
-      .json({ error: "username field cannot be empty" })
+      .json({ error: "username field cannot be empty" });
   }
   if (!password) {
     return response
       .status(400)
-      .json({ error: "password field cannot be empty" })
+      .json({ error: "password field cannot be empty" });
   }
   if (username.length < 3) {
     return response
       .status(400)
-      .json({ error: "username must consist of at least 3 characters" })
+      .json({ error: "username must consist of at least 3 characters" });
   }
   if (password.length < 3) {
     return response
       .status(400)
-      .json({ error: "password must consist of at least 3 characters" })
+      .json({ error: "password must consist of at least 3 characters" });
   }
 
-  const exists = await User.findOne({ username })
+  const exists = await User.findOne({ username });
   if (exists) {
     return response.status(400).json({
       error: "username is already taken",
-    })
+    });
   }
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
     username,
     name,
     passwordHash,
-  })
+  });
 
-  const savedUser = await user.save()
+  const savedUser = await user.save();
 
-  response.status(201).json(savedUser)
-})
+  response.status(201).json(savedUser);
+});
 
-module.exports = usersRouter
+module.exports = usersRouter;
--- osa4/blogilista/models/user.js
+++ osa4/blogilista/models/user.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true, minlength: 3 },
   name: String,
   passwordHash: { type: String, required: true, minlength: 3 },
   blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Blog" }],
-})
+});
 
 userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-    delete returnedObject.passwordHash
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
   },
-})
+});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
